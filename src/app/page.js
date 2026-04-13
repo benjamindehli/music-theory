31fbdfabb5fecc86b3b217aa6d18b352e313c89a
Benjamin Dehli <superelg@gmail.com>
2026-04-13 20:36:36 +0200

    Simplify homepage chord navigation
    Replaces the exhaustive list of individual chord links on the home page with a single link to the chords index page. This declutters the landing page and centralizes chord discovery within the dedicated index.

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,16 @@
-import { getAllNotes, getAllChordTypes, getSlugForChord } from "@/lib/api";
 import Link from "next/link";
 
 export default function Home() {
-    const notes = getAllNotes();
-    const chordTypes = getAllChordTypes();
     return (
         <div>
             <main>
                 <section>
                     <h2>Chords</h2>
-                    {chordTypes
-                        .sort((a, b) => a.name.localeCompare(b.name))
-                        .map((chordType) => (
-                            <section key={`${chordType.name}`}>
-                                <h3>{chordType.name}</h3>
-                                <ul>
-                                    {notes.map((note) => {
-                                        const chordSlug = getSlugForChord(note, chordType);
-                                        return (
-                                            <li key={chordSlug}>
-                                                <Link href={`/chords/${chordSlug}`}>
-                                                    {note.name} {chordType.name}
-                                                </Link>
-                                            </li>
-                                        );
-                                    })}
-                                </ul>
-                            </section>
-                        ))}
+                    <ul>
+                        <li>
+                            <Link href="/chords">All Chords</Link>
+                        </li>
+                    </ul>
                 </section>
             </main>
         </div>
